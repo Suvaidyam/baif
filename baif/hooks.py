d555d27a39f1886311a0599456095b99c89cc85c
bittu9835 <bk2671111@gmail.com>
2025-08-01 08:44:21 +0000

    add validation and bank feature for ngo

--- baif/hooks.py
+++ baif/hooks.py
@@ -108,7 +108,8 @@ fixtures = [
 
 # include js in doctype views
 doctype_js = {
-    "Grant" : "public/js/grant.js"
+    "Grant" : "public/js/grant.js",
+    "NGO" : "public/js/ngo.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
